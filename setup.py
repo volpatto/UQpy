73b762e2701d385059821799e8bff03f1639b49b
Dimitris Giovanis <30946950+dgiovanis@users.noreply.github.com>
2019-04-15 17:28:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,9 @@ setup(
     version="2.0.1",
     url='https://github.com/SURGroup/UQpy',
     authors="Michael D. Shields, Dimitris G. Giovanis",
-    author_emails="michael.shields@jhu.edu, dgiovan1@jhu.edu",
+    author_emails="UQpy.info@gmail.com",
     license='MIT',
-    platforms=[
-        'OSX','Windows', 'Linux'
-    ]
+    platforms=['OSX','Windows', 'Linux'],
     packages=find_packages("src"),
     package_dir={"": "src"},
     package_data={"":["*.pdf"]},
