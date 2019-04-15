2ed731d4df060955adf68cb834ee81d5a50206d6
dgiov16 <moonwalkerd@hotmail.com>
2019-04-15 17:25:19 -0400

    fix bug in setup

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(
     version="1.1.0",
     url='https://github.com/SURGroup/UQpy',
     authors="Michael D. Shields, Dimitris G. Giovanis",
-    author_emails="michael.shields@jhu.edu, dgiovan1@jhu.edu",
+    author_emails="UQpy.info@gmail.com",
     license='MIT',
-    platforms='OSX',
+    platforms=['OSX', 'Windows', 'Linux'],
     packages=find_packages("src"),
     package_dir={"": "src"},
-    package_data={"":["*.pdf"]},
+    package_data={"": ["*.pdf"]},
     install_requires=[
         "numpy", "scipy", "matplotlib", "scikit-learn", 'fire'
     ],
